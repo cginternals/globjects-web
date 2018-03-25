88e05fa92469b93a7d8802c36de492fbcb088832
Stefan Buschmann <stefan.buschmann@hpi.de>
2018-03-25 17:18:24 +0200

    Update copy script

--- copy.js
+++ copy.js
@@ -10,31 +10,31 @@ function copy(base, dest, assets, ignore, force) {
             fs.mkdirSync(dest);
         }
 
-        glob(asset, { cwd: base, strict: true }, function (error, files) {
-            files.forEach(file => {
-                if (ignore.indexOf(file) > -1) {
-                    return;
-                }
+        var files = glob.sync(asset, { cwd: base, strict: true });
+        files.forEach(file => {
+            if (ignore.indexOf(file) > -1) {
+                return;
+            }
 
-                const src = path.join(base, file);
-                const dst = path.join(dest, file);
+            const src = path.join(base, file);
+            const dst = path.join(dest, file);
 
-                if (!fs.existsSync(path.dirname(dst))) {
-                    fs.mkdirSync(path.dirname(dst));
-                }
+            if (!fs.existsSync(path.dirname(dst))) {
+                fs.mkdirSync(path.dirname(dst));
+            }
 
-                if (force === false && fs.existsSync(dst) && fs.statSync(dst).mtime >= fs.statSync(src).mtime) {
-                    return;
-                }
+            if (force === false && fs.existsSync(dst) && fs.statSync(dst).mtime >= fs.statSync(src).mtime) {
+                return;
+            }
 
-                if (!fs.lstatSync(src).isDirectory()) {
-                    fs.createReadStream(src).pipe(fs.createWriteStream(dst));
+            if (!fs.lstatSync(src).isDirectory()) {
+                fs.copyFileSync(src, dst);
 
-                    console.log('emitted:', dst);
-                }
-            });
+                console.log('emitted:', dst);
+            }
         });
     });
+
 }
 
 module.exports = copy;
